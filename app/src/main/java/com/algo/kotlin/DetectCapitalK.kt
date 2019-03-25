93828a3ea381bc6b653bb6da42cba7975e549e9d
prince Midha <prince.midha@wynk.in>
2019-03-25 11:34:51 +0530

    DetectCapitalK

---
+++ app/src/main/java/com/algo/kotlin/DetectCapitalK.kt
@@ -0,0 +1,39 @@
+package com.algo.kotlin
+
+/**
+Given a word, you need to judge whether the usage of capitals in it is right or not.
+
+We define the usage of capitals in a word to be right when one of the following cases holds:
+
+All letters in this word are capitals, like "USA".
+All letters in this word are not capitals, like "leetcode".
+Only the first letter in this word is capital if it has more than one letter, like "Google".
+Otherwise, we define that this word doesn't use capitals in a right way.
+
+Example 1:
+Input: "USA"
+Output: True
+
+Example 2:
+Input: "FlaG"
+Output: False
+ */
+
+fun main() {
+    println(isCapital(null))
+    println(isCapital("Google"))
+    println(isCapital("GOOGLE"))
+    println(isCapital("google"))
+    println(isCapital(""))
+}
+
+fun isCapital(text: String?): Boolean {
+    if (text.isNullOrEmpty()) return false
+    var count = 0
+    text.forEach {
+        if (it.isUpperCase() && it.isLetter()) {
+            count++
+        }
+    }
+    return count == text.length || count == 0 || (count == 1 && text[0].isUpperCase())
+}
